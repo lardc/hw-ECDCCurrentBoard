3c102a804db6d04978f86d9cc6de0642ab1f52d6
Mgainullin <m.gainullin@proton-electrotex.com>
2020-07-17 16:05:43 +0300

    Добавил файлы для описания функций управления каналами токов

---
+++ Firmware/Source/Controller/CurrentControl.c
@@ -0,0 +1,12 @@
+// Header
+#include "CurrentControl.h"
+// Includes
+#include "Board.h"
+#include "LowLevel.h"
+#include "Global.h"
+#include "DataTable.h"
+#include "DeviceObjectDictionary.h"
+
+// Forward functions
+
+
---
+++ Firmware/Source/Controller/CurrentControl.h
@@ -0,0 +1,7 @@
+#ifndef __CURRENTCONTROL_H_
+#define __CURRENTCONTROL_H_
+
+// Include
+#include "stdinc.h"
+
+#endif /* __CURRENTCONTROL_H_ */
