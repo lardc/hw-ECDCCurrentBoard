35b3a17769ae390abb3de7c769176c3b8444d5a1
Mgainullin <m.gainullin@proton-electrotex.com>
2020-07-17 19:58:00 +0300

    В CurrentControl добавил функции настройки ЦАП и выбора канала тока

--- Firmware/Source/Controller/CurrentControl.c
+++ Firmware/Source/Controller/CurrentControl.c
@@ -8,5 +8,63 @@
 #include "DeviceObjectDictionary.h"
 
 // Forward functions
+uint16_t ÑÑ_ItoDAC(float Current)
+{
+	float K = (float)DataTable[REG_DAC_I_SET_K] / 1000;
+	float Offset = (float)((int16_t)DataTable[REG_DAC_I_SET_OFFSET]);
+	
+	uint32_t result = Current * K + Offset;
+	return (result > DAC_RESOLUTION) ? DAC_RESOLUTION : result;
+}
+//---------------------
 
+void ÑÑ_SetCurrent(float Current)
+{
+	if(Current <= I_RANGE_2A)
+	{
+		uint16_t Data = CC_ItoDAC(Current) & ~DAC_CHANNEL_B;
+		LL_WriteDACx(Data, GPIO_LDAC1);
+	}
+	else if((Current > I_RANGE_2A) && (Current <= I_RANGE_20A))
+	{
+		uint16_t Data = CC_ItoDAC(Current) | DAC_CHANNEL_B;
+		LL_WriteDACx(Data, GPIO_LDAC1);
+	}
+	else if((Current >= I_RANGE_20A) && (Current <= I_RANGE_270A))
+	{
+		uint16_t Data = CC_ItoDAC(Current) & ~DAC_CHANNEL_B;
+		LL_WriteDACx(Data, GPIO_LDAC2);
+	}
+}
+//---------------------
+
+void CC_EnableCurrentChannel(float Current)
+{
+	LL_EnableRange20MA(FALSE);
+	LL_EnableRange200MA(FALSE);
+	LL_EnableRange2A(FALSE);
+	LL_EnableRange20A(FALSE);
+	LL_EnableRange270A(FALSE);
+	
+	if(Current <= I_RANGE_20MA)
+	{
+		LL_EnableRange20MA(TRUE);
+	}
+	else if((Current > I_RANGE_20MA) && (Current <= I_RANGE_200MA))
+	{
+		LL_EnableRange200MA(TRUE);
+	}
+	else if((Current > I_RANGE_200MA) && (Current <= I_RANGE_2A))
+	{
+		LL_EnableRange2A(TRUE);
+	}
+	else if((Current > I_RANGE_2A) && (Current <= I_RANGE_20A))
+	{
+		LL_EnableRange20A(TRUE);
+	}
+	else if((Current > I_RANGE_20A) && (Current <= I_RANGE_270A))
+	{
+		LL_EnableRange270A(TRUE);
+	}
+}
 
--- Firmware/Source/Controller/CurrentControl.h
+++ Firmware/Source/Controller/CurrentControl.h
@@ -4,4 +4,11 @@
 // Include
 #include "stdinc.h"
 
+uint16_t DAC_CHANNEL_B = 0x8000;
+
+// Functions
+uint16_t ÑÑ_ItoDAC(float Current);
+void ÑÑ_SetCurrent(float Current);
+void CC_EnableCurrentChannel(float Current);
+
 #endif /* __CURRENTCONTROL_H_ */
