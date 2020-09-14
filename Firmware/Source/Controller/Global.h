3501917d722c4738119dc55c54deecb827a68dd5
Mgainullin <m.gainullin@proton-electrotex.com>
2020-09-14 12:14:11 +0300

    Добавил длинну массива измерений и дефайны end point

--- Firmware/Source/Controller/Global.h
+++ Firmware/Source/Controller/Global.h
@@ -5,13 +5,16 @@
 // 
 #define	SCCI_TIMEOUT_TICKS		1000	// Таймаут интерфейса SCCI (в мс)
 #define EP_WRITE_COUNT			0		// Количество массивов для записи
-#define EP_COUNT				0		// Количество массивов для чтения
+#define EP_COUNT				2		// Количество массивов для чтения
+#define VALUES_x_SIZE			1000	// Размер выходных массивов
 #define ENABLE_LOCKING			FALSE	// Защита NV регистров паролем
+#define PULSES_MAX				10		// Максимальное количество импульсов на измерение
 
 // Временные параметры
 #define TIME_LED_BLINK			500		// Мигание светодиодом (в мс)
 #define TIME_EXT_LED_BLINK		500		// Время загорания внешнего светодиода (в мс)
-#define TIME_BAT_CHARGE			1000	// Время заряда батареи  (в мс)
+#define TIME_BAT_CHARGE			5000	// Время заряда батареи  (в мс)
+#define Pulse2PulsePause		2000	// Время ежду импульсами (в мс)
 
 // Параметры АЦП
 #define V_ADC1_CH				1		// Номер канала АЦП V_DUT
@@ -36,6 +39,17 @@
 #define I_RANGE_270A			270000	// Диапазон до 270А
 
 #define END_CURRENT_PULSE		0		// Значение задания тока 0
+#define LOCK_ANALOG_CH			0		// Отключение ОС и каналов измерения тока
+
+#define I_ANALOG_2A				0x0024	// Маска выбора включения ОС и измерения канала до 2А
+#define I_ANALOG_20A			0x0012	// Маска выбора включения ОС и измерения канала до 20А
+#define I_ANALOG_270A			0x0009	// Маска выбора включения ОС и измерения канала до 270А
+
+#define I_SHUNT_20MA			100000	// Шунт 100000 мОм до 20мА
+#define I_SHUNT_200MA			10000	// Шунт 10000 мОм до 200мА
+#define I_SHUNT_2A				500		// Шунт 500 мОм до 2А
+#define I_SHUNT_20A				50		// Шунт 50 мОм до 20А
+#define I_SHUNT_270A			0.5		// Шунт 0.5 мОм до 270А
 
 //Параметры измерения напряжения
 #define V_RANGE_30MV			30		// Диапазон до 20мА
