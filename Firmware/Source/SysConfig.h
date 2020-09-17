847dd68942baaed1ab03b5627b46bfc98a7f8645
Mgainullin <m.gainullin@proton-electrotex.com>
2020-09-17 09:35:12 +0300

    Увеличил такт работы для таймера АЦП

--- Firmware/Source/SysConfig.h
+++ Firmware/Source/SysConfig.h
@@ -18,9 +18,8 @@
 // ----------------------------------------------
 
 // Timers
-#define TIMER6_uS                		0.5f		// Таймер тактирования АЦП
+#define TIMER6_uS                		1			// Таймер тактирования АЦП
 #define TIMER7_uS						1000		// в мкс
-#define TIMER15_uS						10000		// в мкс
 // ----------------------------------------------
 
 // CAN
@@ -33,6 +32,5 @@
 #define SPI2_BAUDRATE_BITS				0x5
 #define SPI2_LSB_FIRST					false
 // ----------------------------------------------
-
 
 #endif // __SYSCONFIG_H
