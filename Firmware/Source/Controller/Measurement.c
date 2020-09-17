27b01c38cf00d6a7a73c3ad99fed02118057e797
Mgainullin <m.gainullin@proton-electrotex.com>
2020-09-17 07:53:18 +0300

    Добавил функцию выбора канала измерения для тока и напряжения

--- Firmware/Source/Controller/Measurement.c
+++ Firmware/Source/Controller/Measurement.c
@@ -4,8 +4,8 @@
 #include "Global.h"
 #include "ZwADC.h"
 
-void MEASURE_ConvertRawArray(volatile uint16_t *InputArray, float *OutputArray, uint16_t ArrayOffset,
-		uint16_t DataLength, uint16_t RegisterOffset, uint16_t RegisterK, uint16_t RegisterP0, uint16_t RegisterP1,
+void MEASURE_ConvertRawArray(volatile uint16_t *InputArray, float *OutputArray,uint16_t DataLength,
+		uint16_t RegisterOffset, uint16_t RegisterK, uint16_t RegisterP0, uint16_t RegisterP1,
 		uint16_t RegisterP2)
 {
 	uint16_t i;
@@ -22,41 +22,63 @@ void MEASURE_ConvertRawArray(volatile uint16_t *InputArray, float *OutputArray, 
 	{
 		tmp = ((float)InputArray[i] - Offset) * ADC_REF_VOLTAGE / ADC_RESOLUTION * K;
 		tmp = tmp * tmp * P2 + tmp * P1 + P0;
-		OutputArray[i * 2 + ArrayOffset] = (tmp > 0) ? tmp : 0;
+		OutputArray[i] = (tmp > 0) ? tmp : 0;
 	}
 }
 //---------------------
 
 void MEASURE_ReadCurrent2A(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertRawArray(InputArray, OutputArray, 0, DataLength, REG_ADC_ID2A_OFFSET, REG_ADC_ID2A_K,
-			REG_ADC_ID2A_FINE_P0, REG_ADC_ID2A_FINE_P1, REG_ADC_ID2A_FINE_P2);
+	MEASURE_ConvertRawArray(InputArray, OutputArray, DataLength, REG_ADC_ID2A_OFFSET, REG_ADC_ID2A_K,
+	REG_ADC_ID2A_FINE_P0, REG_ADC_ID2A_FINE_P1, REG_ADC_ID2A_FINE_P2);
 }
 
 void MEASURE_ReadCurrent270A(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertRawArray(InputArray, OutputArray, 0, DataLength, REG_ADC_ID270A_OFFSET, REG_ADC_ID270A_K,
-			REG_ADC_ID270A_FINE_P0, REG_ADC_ID270A_FINE_P1, REG_ADC_ID270A_FINE_P2);
+	MEASURE_ConvertRawArray(InputArray, OutputArray, DataLength, REG_ADC_ID270A_OFFSET, REG_ADC_ID270A_K,
+	REG_ADC_ID270A_FINE_P0, REG_ADC_ID270A_FINE_P1, REG_ADC_ID270A_FINE_P2);
 }
 
 void MEASURE_ReadVoltage250mV(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertRawArray(InputArray, OutputArray, 1, DataLength, REG_ADC_VD250MV_OFFSET, REG_ADC_VD250MV_K,
-			REG_ADC_VD250MV_FINE_P0, REG_ADC_VD250MV_FINE_P1, REG_ADC_VD250MV_FINE_P2);
+	MEASURE_ConvertRawArray(InputArray, OutputArray, DataLength, REG_ADC_VD250MV_OFFSET, REG_ADC_VD250MV_K,
+	REG_ADC_VD250MV_FINE_P0, REG_ADC_VD250MV_FINE_P1, REG_ADC_VD250MV_FINE_P2);
 }
 
 void MEASURE_ReadVoltage11V(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength)
 {
-	MEASURE_ConvertRawArray(InputArray, OutputArray, 1, DataLength, REG_ADC_VD11V_OFFSET, REG_ADC_VD11V_K,
-			REG_ADC_VD11V_FINE_P0, REG_ADC_VD11V_FINE_P1, REG_ADC_VD11V_FINE_P2);
+	MEASURE_ConvertRawArray(InputArray, OutputArray, DataLength, REG_ADC_VD11V_OFFSET, REG_ADC_VD11V_K,
+	REG_ADC_VD11V_FINE_P0, REG_ADC_VD11V_FINE_P1, REG_ADC_VD11V_FINE_P2);
 }
 
 uint16_t MEASURE_GetBatteryVoltage()
 {
 	float Offset = (float)((int16_t)DataTable[REG_ADC_VBAT_OFFSET]);
 	float K = (float)DataTable[REG_ADC_VBAT_K] / 1000;
-	float result = ((float)ADC_Measure(ADC2, VBAT_ADC2_CH) - Offset) * ADC_REF_VOLTAGE / ADC_RESOLUTION * K;
+	float result = ((float)ADC_Measure(ADC3, VBAT_ADC3_CH) - Offset) * ADC_REF_VOLTAGE / ADC_RESOLUTION * K;
 	
 	return (result > 0) ? result : 0;
 }
 //------------------------------------------------
+
+void MEASURE_MeasuremenChannel(float Current, float Voltage)
+{
+	if(Current <= I_RANGE_2A)
+	{
+		MEASURE_ReadCurrent2A(CONTROL_DUTCurrentRaw, CONTROL_ValuesDUTCurrent, VALUES_OUT_SIZE);
+	}
+	else
+	{
+		MEASURE_ReadCurrent270A(CONTROL_DUTCurrentRaw, CONTROL_ValuesDUTCurrent, VALUES_OUT_SIZE);
+	}
+
+	if(Voltage <= V_RANGE_250MV)
+	{
+		MEASURE_ReadVoltage250mV(CONTROL_DUTVoltageRaw, CONTROL_ValuesDUTVoltage, VALUES_OUT_SIZE);
+	}
+	else
+	{
+		MEASURE_ReadVoltage11V(CONTROL_DUTVoltageRaw, CONTROL_ValuesDUTVoltage, VALUES_OUT_SIZE);
+	}
+}
+//-----------------------------------------------
--- Firmware/Source/Controller/Measurement.h
+++ Firmware/Source/Controller/Measurement.h
@@ -3,22 +3,14 @@
 
 #include "stdinc.h"
 
-void MEASURE_ConvertRawArray(volatile uint16_t *InputArray, float *OutputArray, uint16_t ArrayOffset, uint16_t DataLength,
+void MEASURE_ConvertRawArray(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength,
 		  uint16_t RegisterOffset, uint16_t RegisterK, uint16_t RegisterP0, uint16_t RegisterP1, uint16_t RegisterP2);
-
 void MEASURE_ReadCurrent2A(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength);
-
 void MEASURE_ReadCurrent270A(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength);
-
 void MEASURE_ReadVoltage250mV(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength);
-
 void MEASURE_ReadVoltage11V(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength);
-
 uint16_t MEASURE_GetBatteryVoltage();
+void MEASURE_MeasuremenChannel(float Current, float Voltage);
 //------------------------------------------------
-
-
-//------------------------------------------------
-
 
 #endif // __MEASUREMENT_H
