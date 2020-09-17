cb9b256462ec95840dd93985143ca14300b42fc7
Mgainullin <m.gainullin@proton-electrotex.com>
2020-09-17 07:47:16 +0300

    добавил библиотеку обработки логики

---
+++ Firmware/Source/Controller/Logic.h
@@ -0,0 +1,12 @@
+#ifndef __LOGIC_H
+#define __LOGIC_H
+
+// Include
+#include "stdinc.h"
+
+// Functions
+void LOGIC_ClearDataArrays();
+void LOGIC_PulseConfig();
+void LOGIC_CacheVariables();
+
+#endif // __LOGIC_H
