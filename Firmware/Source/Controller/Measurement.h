4ce1814b83ef477092d810926d8dee41092d486e
Mgainullin <m.gainullin@proton-electrotex.com>
2020-09-18 14:03:15 +0300

    Поправил типы переменных

--- Firmware/Source/Controller/Measurement.h
+++ Firmware/Source/Controller/Measurement.h
@@ -4,7 +4,7 @@
 #include "stdinc.h"
 
 void MEASURE_ConvertRawArray(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength,
-		  uint16_t RegisterOffset, uint16_t RegisterK, uint16_t RegisterP0, uint16_t RegisterP1, uint16_t RegisterP2);
+		uint16_t RegisterOffset, uint16_t RegisterK, uint16_t RegisterP0, uint16_t RegisterP1, uint16_t RegisterP2);
 void MEASURE_ReadCurrent2A(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength);
 void MEASURE_ReadCurrent270A(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength);
 void MEASURE_ReadVoltage250mV(volatile uint16_t *InputArray, float *OutputArray, uint16_t DataLength);
