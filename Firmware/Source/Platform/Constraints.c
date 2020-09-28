ed7b33aab853a7018655d86bbc3292418c689061
Mgainullin <m.gainullin@proton-electrotex.com>
2020-09-28 14:01:47 +0300

    дифайн диапазонов

--- Firmware/Source/Platform/Constraints.c
+++ Firmware/Source/Platform/Constraints.c
@@ -7,68 +7,68 @@
 
 // Constants
 //
-const TableItemConstraint NVConstraint[DATA_TABLE_NV_SIZE] = {{0, INT16U_MAX, 0},	// 0
-		{0, INT16U_MAX, 0},															// 1
-		{0, INT16U_MAX, 0},															// 2
-		{0, INT16U_MAX, 0},															// 3
-		{0, INT16U_MAX, 0},															// 4
-		{0, INT16U_MAX, 0},															// 5
-		{0, INT16U_MAX, 0},															// 6
-		{0, INT16U_MAX, 0},															// 7
-		{0, INT16U_MAX, 0},															// 8
-		{0, INT16U_MAX, 0},															// 9
-		{0, INT16U_MAX, 0},															// 10
-		{0, INT16U_MAX, 0},															// 11
-		{0, INT16U_MAX, 0},															// 12
-		{0, INT16U_MAX, 0},															// 13
-		{0, INT16U_MAX, 0},															// 14
-		{0, INT16U_MAX, 0},															// 15
-		{0, INT16U_MAX, 0},															// 16
-		{0, INT16U_MAX, 0},															// 17
-		{0, INT16U_MAX, 0},															// 18
-		{0, 0, 0},																	// 19
-		{0, 0, 0},																	// 20
-		{0, 0, 0},																	// 21
-		{0, 0, 0},																	// 22
-		{0, 0, 0},																	// 23
-		{0, INT16U_MAX, 0},															// 24
-		{0, INT16U_MAX, 0},															// 25
-		{0, INT16U_MAX, 0},															// 26
-		{0, INT16U_MAX, 0},															// 27
-		{0, 0, 0},																	// 28
-		{0, 0, 0},																	// 29
-		{0, 0, 0},																	// 30
-		{0, 0, 0},																	// 31
-		{0, 0, 0},																	// 32
-		{0, 0, 0},																	// 33
-		{0, 0, 0},																	// 34
-		{0, 0, 0},																	// 35
-		{0, 0, 0},																	// 36
-		{0, 0, 0},																	// 37
-		{0, 0, 0},																	// 38
-		{0, 0, 0},																	// 39
-		{0, 0, 0},																	// 40
-		{0, 0, 0},																	// 41
-		{0, 0, 0},																	// 42
-		{0, 0, 0},																	// 43
-		{0, 0, 0},																	// 44
-		{0, 0, 0},																	// 45
-		{0, 0, 0},																	// 46
-		{0, 0, 0},																	// 47
-		{0, 0, 0},																	// 48
-		{0, 0, 0},																	// 49
-		{0, 0, 0},																	// 50
-		{0, 0, 0},																	// 51
-		{0, 0, 0},																	// 52
-		{0, 0, 0},																	// 53
-		{0, 0, 0},																	// 54
-		{0, 0, 0},																	// 55
-		{0, 0, 0},																	// 56
-		{0, 0, 0},																	// 57
-		{0, 0, 0},																	// 58
-		{0, 0, 0},																	// 59
-		{0, 0, 0},																	// 60
-		{0, 0, 0},																	// 61
+const TableItemConstraint NVConstraint[DATA_TABLE_NV_SIZE] = {{0, 0, 0},			// 0
+		{0, 0, 0},																	// 1
+		{VBAT_OFFSET_MIN, VBAT_OFFSET_MAX, VBAT_OFFSET_DEF},						// 2
+		{VBAT_K_MIN, VBAT_K_MAX, VBAT_K_DEF},										// 3
+		{VD30MV_OFFSET_MIN, VD30MV_OFFSET_MAX, VD30MV_OFFSET_DEF},					// 4
+		{VD30MV_K_MIN, VD30MV_K_MAX, VD30MV_K_DEF},									// 5
+		{VD30MV_P0_MIN, VD30MV_P0_MAX, VD30MV_P0_DEF},								// 6
+		{VD30MV_P1_MIN, VD30MV_P1_MAX, VD30MV_P1_DEF},								// 7
+		{VD30MV_P2_MIN, VD30MV_P2_MAX, VD30MV_P2_DEF},								// 8
+		{VD250MV_OFFSET_MIN, VD250MV_OFFSET_MAX, VD250MV_OFFSET_DEF},				// 9
+		{VD250MV_K_MIN, VD250MV_K_MAX, VD250MV_K_DEF},								// 10
+		{VD250MV_P0_MIN, VD250MV_P0_MAX, VD250MV_P0_DEF},							// 11
+		{VD250MV_P1_MIN, VD250MV_P1_MAX, VD250MV_P1_DEF},							// 12
+		{VD250MV_P2_MIN, VD250MV_P2_MAX, VD250MV_P2_DEF},							// 13
+		{VD1500MV_OFFSET_MIN, VD1500MV_OFFSET_MAX, VD1500MV_OFFSET_DEF},			// 14
+		{VD1500MV_K_MIN, VD1500MV_K_MAX, VD1500MV_K_DEF},							// 15
+		{VD1500MV_P0_MIN, VD1500MV_P0_MAX, VD1500MV_P0_DEF},						// 16
+		{VD1500MV_P1_MIN, VD1500MV_P1_MAX, VD1500MV_P1_DEF},						// 17
+		{VD1500MV_P2_MIN, VD1500MV_P2_MAX, VD1500MV_P2_DEF},						// 18
+		{VD11V_OFFSET_MIN, VD11V_OFFSET_MAX, VD11V_OFFSET_DEF},						// 19
+		{VD11V_K_MIN, VD11V_K_MAX, VD11V_K_DEF},									// 20
+		{VD11V_P0_MIN, VD11V_P0_MAX, VD11V_P0_DEF},									// 21
+		{VD11V_P1_MIN, VD11V_P1_MAX, VD11V_P1_DEF},									// 22
+		{VD11V_P2_MIN, VD11V_P2_MAX, VD11V_P2_DEF},									// 23
+		{ID20MA_OFFSET_MIN, ID20MA_OFFSET_MAX, ID20MA_OFFSET_DEF},					// 24
+		{ID20MA_K_MIN, ID20MA_K_MAX, ID20MA_K_DEF},									// 25
+		{ID20MA_P0_MIN, ID20MA_P0_MAX, ID20MA_P0_DEF},								// 26
+		{ID20MA_P1_MIN, ID20MA_P1_MAX, ID20MA_P1_DEF},								// 27
+		{ID20MA_P2_MIN, ID20MA_P2_MAX, ID20MA_P2_DEF},								// 28
+		{ID200MA_OFFSET_MIN, ID200MA_OFFSET_MAX, ID200MA_OFFSET_DEF},				// 29
+		{ID200MA_K_MIN, ID200MA_K_MAX, ID200MA_K_DEF},								// 30
+		{ID200MA_P0_MIN, ID200MA_P0_MAX, ID200MA_P0_DEF},							// 31
+		{ID200MA_P1_MIN, ID200MA_P1_MAX, ID200MA_P1_DEF},							// 32
+		{ID200MA_P2_MIN, ID200MA_P2_MAX, ID200MA_P2_DEF},							// 33
+		{ID2A_OFFSET_MIN, ID2A_OFFSET_MAX, ID2A_OFFSET_DEF},						// 34
+		{ID2A_K_MIN, ID2A_K_MAX, ID2A_K_DEF},										// 35
+		{ID2A_P0_MIN, ID2A_P0_MAX, ID2A_P0_DEF},									// 36
+		{ID2A_P1_MIN, ID2A_P1_MAX, ID2A_P1_DEF},									// 37
+		{ID2A_P2_MIN, ID2A_P2_MAX, ID2A_P2_DEF},									// 38
+		{ID20A_OFFSET_MIN, ID20A_OFFSET_MAX, ID20A_OFFSET_DEF},						// 39
+		{ID20A_K_MIN, ID20A_K_MAX, ID20A_K_DEF},									// 40
+		{ID20A_P0_MIN, ID20A_P0_MAX, ID20A_P0_DEF},									// 41
+		{ID20A_P1_MIN, ID20A_P1_MAX, ID20A_P1_DEF},									// 42
+		{ID20A_P2_MIN, ID20A_P2_MAX, ID20A_P2_DEF},									// 43
+		{ID270A_OFFSET_MIN, ID270A_OFFSET_MAX, ID270A_OFFSET_DEF},					// 44
+		{ID270A_K_MIN, ID270A_K_MAX, ID270A_K_DEF},									// 45
+		{ID270A_P0_MIN, ID270A_P0_MAX, ID270A_P0_DEF},								// 46
+		{ID270A_P1_MIN, ID270A_P1_MAX, ID270A_P1_DEF},								// 47
+		{ID270A_P2_MIN, ID270A_P2_MAX, ID270A_P2_DEF},								// 48
+		{DAC_I20MA_OFFSET_MIN, DAC_I20MAT_OFFSET_MAX, DAC_I20MA_OFFSET_DEF},		// 49
+		{DAC_I20MA_K_MIN, DAC_I20MA_K_MAX, DAC_I20MA_K_DEF},						// 50
+		{DAC_I200MA_OFFSET_MIN, DAC_I200MA_OFFSET_MAX, DAC_I200MA_OFFSET_DEF},		// 51
+		{DAC_I200MA_K_MIN, DAC_I200MA_K_MAX, DAC_I200MA_K_DEF},						// 52
+		{DAC_I2A_OFFSET_MIN, DAC_I2A_OFFSET_MAX, DAC_I2A_OFFSET_DEF},				// 53
+		{DAC_I2A_K_MIN, DAC_I2A_K_MAX, DAC_I2A_K_DEF},								// 54
+		{DAC_I20A_OFFSET_MIN, DAC_I20A_OFFSET_MAX, DAC_I20A_OFFSET_DEF},			// 53
+		{DAC_I20A_K_MIN, DAC_I20A_K_MAX, DAC_I20A_K_DEF},							// 54
+		{DAC_I270A_OFFSET_MIN, DAC_I270A_OFFSET_MAX, DAC_I270A_OFFSET_DEF},			// 53
+		{DAC_I270A_K_MIN, DAC_I270A_K_MAX, DAC_I270A_K_DEF},						// 54
+		{CTRL_P_COEF_MIN, CTRL_P_COEF_MAX, CTRL_P_COEF_DEF},						// 59
+		{CTRL_I_COEF_MIN, CTRL_I_COEF_MAX, CTRL_I_COEF_DEF},						// 60
+		{PULSE_PAUSE_MIN, PULSE_PAUSE_MAX, PULSE_PAUSE_DEF},						// 61
 		{0, 0, 0},																	// 62
 		{0, 0, 0},																	// 63
 		{0, 0, 0},																	// 64
@@ -137,12 +137,13 @@ const TableItemConstraint NVConstraint[DATA_TABLE_NV_SIZE] = {{0, INT16U_MAX, 0}
 		{0, 1, 1}																	// 127
 };
 
-const TableItemConstraint VConstraint[DATA_TABLE_WP_START - DATA_TABLE_WR_START] = {{BAT_VOLTAGE_MIN, BAT_VOLTAGE_MAX,
-BAT_VOLTAGE_DEF},																	// 128
-		{0, INT16U_MAX, 0},															// 129
-		{0, INT16U_MAX, 0},															// 130
-		{0, INT16U_MAX, 0},															// 131
-		{0, INT16U_MAX, 0},															// 132
+const TableItemConstraint VConstraint[DATA_TABLE_WP_START - DATA_TABLE_WR_START] = {{CURRENT_SETPOINT_LOW_MIN,
+CURRENT_SETPOINT_LOW_MAX,
+CURRENT_SETPOINT_LOW_DEF},																				// 128
+		{CURRENT_SETPOINT_HIGH_MIN, CURRENT_SETPOINT_HIGH_MAX, CURRENT_SETPOINT_HIGH_DEF},						// 129
+		{VOLTAGE_DUT_LIM_MIN, VOLTAGE_DUT_LIM_MAX, VOLTAGE_DUT_LIM_DEF},										// 130
+		{EN_CURRENT_FB_MIN, EN_CURRENT_FB_MAX, EN_CURRENT_FB_DEF},												// 131
+		{0, 0, 0},																	// 132
 		{0, 0, 0},																	// 133
 		{0, 0, 0},																	// 134
 		{0, 0, 0},																	// 135
@@ -150,7 +151,7 @@ BAT_VOLTAGE_DEF},																	// 128
 		{0, 0, 0},																	// 137
 		{0, 0, 0},																	// 138
 		{0, 0, 0},																	// 139
-		{0, INT16U_MAX, 0},															// 140
+		{DBG_DATA_MIN, DBG_DATA_MAX, DBG_DATA_DEF},									// 140
 		{0, 0, 0},																	// 141
 		{0, 0, 0},																	// 142
 		{0, 0, 0},																	// 143
