81dfb66ff1599d7145660b5d17f07a37f9646354
Mgainullin <m.gainullin@proton-electrotex.com>
2020-09-28 09:27:53 +0300

    поменял функции задержки на DELAY_MS() и добавил задержки после коммутации реле в функции выбора канала тока

--- Firmware/Source/Controller/CurrentControl.c
+++ Firmware/Source/Controller/CurrentControl.c
@@ -7,6 +7,8 @@
 #include "DataTable.h"
 #include "DeviceObjectDictionary.h"
 #include "Controller.h"
+#include "Logic.h"
+#include "Measurement.h"
 #include "Delay.h"
 
 // Forward functions
@@ -38,7 +40,7 @@ void CC_SetCurrentMax2A(float Current)
 {
 	uint16_t Data = (uint32_t)CC_ItoDAC(Current) & ~DAC_CHANNEL_B;
 	LL_WriteDACx(Data, GPIO_LDAC1);
-	DELAY_US(15000);
+	DELAY_MS(15);
 	LL_WriteDACx((uint16_t)END_CURRENT_PULSE, GPIO_LDAC1);
 }
 //---------------------
@@ -47,7 +49,7 @@ void CC_SetCurrentMax20A(float Current)
 {
 	uint16_t Data = (uint32_t)CC_ItoDAC(Current) | DAC_CHANNEL_B;
 	LL_WriteDACx(Data, GPIO_LDAC1);
-	DELAY_US(15000);
+	DELAY_MS(15);
 	LL_WriteDACx(((uint16_t)END_CURRENT_PULSE | DAC_CHANNEL_B), GPIO_LDAC1);
 }
 //---------------------
@@ -56,7 +58,7 @@ void CC_SetCurrentMax270A(float Current)
 {
 	uint16_t Data = (uint32_t)CC_ItoDAC(Current) & ~DAC_CHANNEL_B;
 	LL_WriteDACx(Data, GPIO_LDAC2);
-	DELAY_US(15000);
+	DELAY_MS(15);
 	LL_WriteDACx((uint16_t)END_CURRENT_PULSE, GPIO_LDAC2);
 }
 //---------------------
@@ -92,35 +94,46 @@ void CC_EnableCurrentChannel(float Current, float EnableFb)
 	LL_EnableRange200mA(false);
 	LL_EnableRange2A(false);
 	LL_EnableRange20A(false);
+	LL_EnableRange270A(false);
+	DELAY_MS(10);
 	LL_DisableRange270A(true);
+	DELAY_MS(10);
+	LL_DisableRange270A(false);
 	
 	if(Current <= I_RANGE_20MA)
 	{
 		LL_EnableRange20mA(true);
+		DELAY_MS(10);
 		Data = (EnableFb == 1) ? I_ANALOG_2A : (I_ANALOG_2A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 	else if(Current <= I_RANGE_200MA)
 	{
 		LL_EnableRange200mA(true);
+		DELAY_MS(10);
 		Data = (EnableFb == 1) ? I_ANALOG_2A : (I_ANALOG_2A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 	else if(Current <= I_RANGE_2A)
 	{
 		LL_EnableRange2A(true);
+		DELAY_MS(10);
 		Data = (EnableFb == 1) ? I_ANALOG_2A : (I_ANALOG_2A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 	else if(Current <= I_RANGE_20A)
 	{
 		LL_EnableRange20A(true);
+		DELAY_MS(10);
 		Data = (EnableFb == 1) ? I_ANALOG_20A : (I_ANALOG_20A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 	else
 	{
 		LL_EnableRange270A(true);
+		DELAY_MS(10);
+		LL_EnableRange270A(false);
+		DELAY_MS(10);
 		Data = (EnableFb == 0) ? I_ANALOG_270A : (I_ANALOG_270A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
