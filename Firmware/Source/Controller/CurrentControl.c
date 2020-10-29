30da75eb139a34016f649b742bc2262c54e9bffa
Mgainullin <m.gainullin@proton-electrotex.com>
2020-10-29 07:55:32 +0200

    Исправил функцию пересчета задания в данные ЦАП, сделал формирование импульса в зависимости от диапазона тока, добавил сброс реле в функцию выбора канала тока

--- Firmware/Source/Controller/CurrentControl.c
+++ Firmware/Source/Controller/CurrentControl.c
@@ -12,38 +12,12 @@
 #include "Delay.h"
 
 // Forward functions
-uint16_t CC_ItoDAC(float Current)
+uint16_t CC_ItoDAC(float Data, uint16_t RegisterOffset, uint16_t RegisterK)
 {
-	float K;
-	float Offset;
+	float K = (float)DataTable[RegisterK] / 1000000;
+	float Offset = (float)((int16_t)DataTable[RegisterOffset]);
 	
-	if(Current <= I_RANGE_20MA)
-	{
-		K = (float)DataTable[REG_DAC_I20MA_SET_K] / 1000;
-		Offset = (float)((int16_t)DataTable[REG_DAC_I20MA_SET_OFFSET]);
-	}
-	else if(Current <= I_RANGE_200MA)
-	{
-		K = (float)DataTable[REG_DAC_I200MA_SET_K] / 1000;
-		Offset = (float)((int16_t)DataTable[REG_DAC_I200MA_SET_OFFSET]);
-	}
-	else if(Current <= I_RANGE_2A)
-	{
-		K = (float)DataTable[REG_DAC_I2A_SET_K] / 1000;
-		Offset = (float)((int16_t)DataTable[REG_DAC_I2A_SET_OFFSET]);
-	}
-	else if(Current <= I_RANGE_20A)
-	{
-		K = (float)DataTable[REG_DAC_I20A_SET_K] / 1000;
-		Offset = (float)((int16_t)DataTable[REG_DAC_I20A_SET_OFFSET]);
-	}
-	else
-	{
-		K = (float)DataTable[REG_DAC_I270A_SET_K] / 1000;
-		Offset = (float)((int16_t)DataTable[REG_DAC_I270A_SET_OFFSET]);
-	}
-	
-	uint32_t result = Current * K + Offset;
+	uint32_t result = (Data) * K + Offset;
 	return (result > DAC_RESOLUTION) ? DAC_RESOLUTION : result;
 }
 //---------------------
@@ -64,7 +38,7 @@ float CC_CurrentSetup(float Current)
 
 void CC_SetCurrentMax2A(float Current)
 {
-	uint16_t Data = (uint32_t)CC_ItoDAC(Current) & ~DAC_CHANNEL_B;
+	uint16_t Data = (uint32_t)CC_ItoDAC(Current, REG_DAC_I20MA_SET_OFFSET, REG_DAC_I20MA_SET_K) & ~DAC_CHANNEL_B;
 	LL_WriteDACx(Data, GPIO_LDAC1);
 	DELAY_MS(15);
 	LL_WriteDACx((uint16_t)END_CURRENT_PULSE, GPIO_LDAC1);
@@ -73,7 +47,7 @@ void CC_SetCurrentMax2A(float Current)
 
 void CC_SetCurrentMax20A(float Current)
 {
-	uint16_t Data = (uint32_t)CC_ItoDAC(Current) | DAC_CHANNEL_B;
+	uint16_t Data = (uint32_t)CC_ItoDAC(Current, REG_DAC_I20A_SET_OFFSET, REG_DAC_I20A_SET_K) | DAC_CHANNEL_B;
 	LL_WriteDACx(Data, GPIO_LDAC1);
 	DELAY_MS(15);
 	LL_WriteDACx(((uint16_t)END_CURRENT_PULSE | DAC_CHANNEL_B), GPIO_LDAC1);
@@ -82,31 +56,45 @@ void CC_SetCurrentMax20A(float Current)
 
 void CC_SetCurrentMax270A(float Current)
 {
-	uint16_t Data = (uint32_t)CC_ItoDAC(Current) & ~DAC_CHANNEL_B;
+	uint16_t Data = (uint32_t)CC_ItoDAC(Current, REG_DAC_I270A_SET_OFFSET, REG_DAC_I270A_SET_K) & ~DAC_CHANNEL_B;
 	LL_WriteDACx(Data, GPIO_LDAC2);
 	DELAY_MS(15);
 	LL_WriteDACx((uint16_t)END_CURRENT_PULSE, GPIO_LDAC2);
 }
 //---------------------
 
-void CC_SetCurrentPulse(uint16_t OutData, float Current)
+void CC_SetCurrentPulse(float OutData, float Current)
 {
 	uint16_t Data;
 	
-	if(Current <= I_RANGE_2A)
+	if(Current <= I_RANGE_20MA)
 	{
-		Data = (uint32_t)OutData & ~DAC_CHANNEL_B;
+		Data = (OutData == 0) ?
+				((uint32_t)OutData & ~DAC_CHANNEL_B)  : (CC_ItoDAC(OutData, REG_DAC_I20MA_SET_OFFSET, REG_DAC_I20MA_SET_K) & ~DAC_CHANNEL_B);
+		LL_WriteDACx(Data, GPIO_LDAC1);
+	}
+	
+	else if(Current <= I_RANGE_200MA)
+	{
+		Data = (OutData == 0) ?
+				((uint32_t)OutData & ~DAC_CHANNEL_B) : (CC_ItoDAC(OutData, REG_DAC_I200MA_SET_OFFSET, REG_DAC_I200MA_SET_K) & ~DAC_CHANNEL_B);
+		LL_WriteDACx(Data, GPIO_LDAC1);
+	}
+	
+	else if(Current <= I_RANGE_2A)
+	{
+		Data = (OutData == 0) ? ((uint32_t)OutData & ~DAC_CHANNEL_B) : (CC_ItoDAC(OutData, REG_DAC_I2A_SET_OFFSET, REG_DAC_I2A_SET_K) & ~DAC_CHANNEL_B);
 		LL_WriteDACx(Data, GPIO_LDAC1);
 	}
 	
 	else if(Current <= I_RANGE_20A)
 	{
-		Data = (uint32_t)OutData | DAC_CHANNEL_B;
+		Data = (OutData == 0) ? ((uint32_t)OutData | DAC_CHANNEL_B) : (CC_ItoDAC(OutData, REG_DAC_I20A_SET_OFFSET, REG_DAC_I20A_SET_K) | DAC_CHANNEL_B);
 		LL_WriteDACx(Data, GPIO_LDAC1);
 	}
 	else
 	{
-		Data = (uint32_t)OutData & ~DAC_CHANNEL_B;
+		Data = (OutData == 0) ? ((uint32_t)OutData & ~DAC_CHANNEL_B) : (CC_ItoDAC(OutData, REG_DAC_I270A_SET_OFFSET, REG_DAC_I270A_SET_K) & ~DAC_CHANNEL_B);
 		LL_WriteDACx(Data, GPIO_LDAC2);
 	}
 }
@@ -116,51 +104,44 @@ void CC_EnableCurrentChannel(float Current, float EnableFb)
 {
 	uint16_t Data;
 	
-	LL_EnableRange20mA(false);
-	LL_EnableRange200mA(false);
-	LL_EnableRange2A(false);
-	LL_EnableRange20A(false);
-	LL_EnableRange270A(false);
-	DELAY_MS(10);
-	LL_DisableRange270A(true);
-	DELAY_MS(10);
-	LL_DisableRange270A(false);
-	
 	if(Current <= I_RANGE_20MA)
 	{
 		LL_EnableRange20mA(true);
-		DELAY_MS(10);
+		DELAY_MS(12);
 		Data = (EnableFb == 1) ? I_ANALOG_2A : (I_ANALOG_2A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 	else if(Current <= I_RANGE_200MA)
 	{
 		LL_EnableRange200mA(true);
-		DELAY_MS(10);
+		DELAY_MS(12);
 		Data = (EnableFb == 1) ? I_ANALOG_2A : (I_ANALOG_2A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 	else if(Current <= I_RANGE_2A)
 	{
 		LL_EnableRange2A(true);
-		DELAY_MS(10);
+		DELAY_MS(12);
 		Data = (EnableFb == 1) ? I_ANALOG_2A : (I_ANALOG_2A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 	else if(Current <= I_RANGE_20A)
 	{
 		LL_EnableRange20A(true);
-		DELAY_MS(10);
+		DELAY_MS(12);
 		Data = (EnableFb == 1) ? I_ANALOG_20A : (I_ANALOG_20A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 	else
 	{
-		LL_EnableRange270A(true);
-		DELAY_MS(10);
 		LL_EnableRange270A(false);
-		DELAY_MS(10);
-		Data = (EnableFb == 0) ? I_ANALOG_270A : (I_ANALOG_270A & I_FB_ENABLE);
+		LL_DisableRange270A(false);
+		DELAY_MS(15);
+		LL_EnableRange270A(true);
+		DELAY_MS(15);
+		LL_EnableRange270A(false);
+		LL_DisableRange270A(false);
+		Data = (EnableFb == 1) ? I_ANALOG_270A : (I_ANALOG_270A & I_FB_ENABLE);
 		LL_WriteOutReg(Data);
 	}
 }
@@ -180,7 +161,7 @@ float CC_EnableShuntRes(float Current)
 	}
 	else if(Current <= I_RANGE_2A)
 	{
-		ShuntRes = (float) I_SHUNT_200MA;
+		ShuntRes = (float) I_SHUNT_2A;
 	}
 	else if(Current <= I_RANGE_20A)
 	{
--- Firmware/Source/Controller/CurrentControl.h
+++ Firmware/Source/Controller/CurrentControl.h
@@ -9,12 +9,12 @@
 #define DAC_CHANNEL_B		BIT15
 
 // Functions
-uint16_t CC_ItoDAC(float Current);
+uint16_t CC_ItoDAC(float Data, uint16_t RegisterOffset, uint16_t RegisterK);
 float CC_CurrentSetup(float Current);
 void CC_SetCurrentMax2A(float Current);
 void CC_SetCurrentMax20A(float Current);
 void CC_SetCurrentMax270A(float Current);
-void CC_SetCurrentPulse(uint16_t OutData, float Current);
+void CC_SetCurrentPulse(float OutData, float Current);
 void CC_EnableCurrentChannel(float Current, float EnableFb);
 float CC_EnableShuntRes(float Current);
 //---------------------
