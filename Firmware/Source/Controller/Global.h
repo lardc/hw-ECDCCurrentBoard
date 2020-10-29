b396df397d956e7b7e506c9414ed6d2d0f99f2ca
Mgainullin <m.gainullin@proton-electrotex.com>
2020-10-29 07:37:00 +0200

    Исправил значения диапазонов (мкВ) и значения шунтов

--- Firmware/Source/Controller/Global.h
+++ Firmware/Source/Controller/Global.h
@@ -12,47 +12,48 @@
 #define EP_WRITE_COUNT					0										// Количество массивов для записи
 #define EP_COUNT						4										// Количество массивов для чтения
 #define ENABLE_LOCKING					FALSE									// Защита NV регистров паролем
-#define EP_VALUE						PULSE_BUFFER_SIZE						// азмер выходного массива EP
+#define EP_VALUE						PULSE_BUFFER_SIZE						// Размер выходного массива EP
 
 // Параметры импульса тока
 #define PULSE_DELAY						10000									// Время паузы между импульсами тока, мС
 #define PULSE_TIME_VALUE				10000									// Длительность импульса тока, мкС
 #define PULSE_BUFFER_SIZE				(PULSE_TIME_VALUE / PULSE_DIVIDER)		// Количество точек в буфере формы импульса
+#define OFFSET_BUFFER_SIZE				65										// Оффсет для начала усреднения измерений
 #define SYNC_LINE_HIGHSTATE_TIMEOUT		25										// Таймаут пребывания линии синхронизации в высоком состоянии
-#define BLOCK_MAX_CURRENT				270000									// Максимально возможный ток для канала до 250А (мА/10)
+#define BLOCK_MAX_CURRENT				270000000								// Максимально возможный ток для канала до 250А (мА/10)
 #define PULSE_LITE_START				10										// Количество шагов перед выходом на полку
-#define PULSE_DIVIDER					((VALUES_OUT_SIZE * TIMER6_uS) + TIME_REG_STEP)	// Делитель массива импульса, мс
+#define PULSE_DIVIDER					80										// Делитель массива импульса, мс
 
 // Параметры регулятора
-#define CTRL_FOLLOW_ERR					20										// Максимально допустимая ошибка (в %)
+#define CTRL_FOLLOW_ERR					40										// Максимально допустимая ошибка (в %)
 
 // Временные параметры
 #define TIME_LED_BLINK					500										// Мигание светодиодом (в мс)
 #define TIME_EXT_LED_BLINK				500										// Время загорания внешнего светодиода (в мс)
-#define TIME_BAT_CHARGE					5000									// Время заряда батареи  (в мс)
-#define TIME_REG_STEP					60										// Время работы такта регулятора (в мс)
+#define TIME_BAT_CHARGE					22000									// Время заряда батареи  (в мс)
+#define TIME_REG_STEP					50										// Время работы такта регулятора (в мс)
 
 // Параметры АЦП
 #define V_ADC1_CH						1										// Номер канала АЦП V_DUT
 #define I_ADC2_CH						1										// Номер канала АЦП I_DUT
 #define VBAT_ADC3_CH					12										// Номер канала АЦП батареи
-#define ADC_REF_VOLTAGE					3300.0f									// Опорное напряжение (в мВ)
+#define ADC_REF_VOLTAGE					3000000.0f								// Опорное напряжение (в мкВ)
 #define ADC_RESOLUTION					4095									// Разрешение АЦП
 
 // Параметры ЦАП
-#define DAC_REF_VOLTAGE					3300.0f									// Опорное напряжение (в мВ)
+#define DAC_REF_VOLTAGE					3000000.0f								// Опорное напряжение (в мВ)
 #define DAC_RESOLUTION					4095									// Разрешение ЦАП
 
 // Параметры батареи
-#define BAT_VOLTAGE_THRESHOLD			50										// Пороговое напряжение батареи (в В)
-#define BAT_VOLTAGE_DELTA				5.0f									// Гистерезис поддержания напряжения (в В)
+#define BAT_VOLTAGE_THRESHOLD			40000									// Пороговое напряжение батареи (в мВ)
+#define BAT_VOLTAGE_DELTA				500.0f									// Гистерезис поддержания напряжения (в мВ)
 
 //Параметры формирования тока
-#define I_RANGE_20MA					20										// Диапазон до 20мА (мА)
-#define I_RANGE_200MA					200										// Диапазон до 200мА (мА)
-#define I_RANGE_2A						2000									// Диапазон до 2А (мА)
-#define I_RANGE_20A						20000									// Диапазон до 20А (мА)
-#define I_RANGE_270A					250000									// Диапазон до 250А (мА)
+#define I_RANGE_20MA					20000									// Диапазон до 20мА (мкА)
+#define I_RANGE_200MA					200000									// Диапазон до 200мА (мкА)
+#define I_RANGE_2A						2000000									// Диапазон до 2А (мкА)
+#define I_RANGE_20A						20000000								// Диапазон до 20А (мкА)
+#define I_RANGE_270A					250000000								// Диапазон до 250А (мкА)
 
 #define END_CURRENT_PULSE				0										// Значение задания тока 0
 #define LOCK_ANALOG_CH					0										// Отключение ОС и каналов измерения тока
@@ -62,20 +63,21 @@
 #define I_ANALOG_270A					0x0009									// Маска выбора включения ОС и измерения канала до 270А
 
 #define I_FB_ENABLE						0x0007									// Маска разрешения включения обратной связи
-#define I_MASK_LOW						0x0000FFFF								// Маска младших разрядов тока
-#define I_MASK_HIGH						0xFFFF0000								// Маска старших разрядов тока
+#define IV_MASK_LOW						0x0000FFFF								// Маска младших разрядов тока
+#define IV_MASK_HIGH					0xFFFF0000								// Маска старших разрядов тока
 
-#define I_SHUNT_20MA					100000									// Шунт 100000 мОм до 20мА
-#define I_SHUNT_200MA					10000									// Шунт 10000 мОм до 200мА
-#define I_SHUNT_2A						500										// Шунт 500 мОм до 2А
-#define I_SHUNT_20A						50										// Шунт 50 мОм до 20А
-#define I_SHUNT_270A					0.5										// Шунт 0.5 мОм до 270А
+#define I_SHUNT_20MA					100										// Шунт 100 Ом до 20мА
+#define I_SHUNT_200MA					10										// Шунт 10 Ом до 200мА
+#define I_SHUNT_2A						0.44									// Шунт 0.44 Ом до 2А
+#define I_SHUNT_20A						0.05									// Шунт 0.05 Ом до 20А
+#define I_SHUNT_270A					0.0005									// Шунт 0.0005 Ом до 270А
 
 //Параметры измерения напряжения
-#define V_RANGE_30MV					30										// Диапазон до 20мА
-#define V_RANGE_250MV					250										// Диапазон до 200мА
-#define V_RANGE_1500MV					1500									// Диапазон до 2А
-#define V_RANGE_11V						11000									// Диапазон до 20А
+#define V_RANGE_10MV					10000									// Диапазон до 10мВ (мкВ)
+#define V_RANGE_30MV					30000									// Диапазон до 30мВ (мкВ)
+#define V_RANGE_250MV					250000									// Диапазон до 250мВ (мкВ)
+#define V_RANGE_1500MV					1500000									// Диапазон до 1500мВ (мкВ)
+#define V_RANGE_11V						11000000								// Диапазон до 11В (мкВ)
 
 // Параметры DMA
 #define DMA_ADC_DUT_V_CHANNEL			DMA1_Channel1							// Канал DMA для АЦП напряжения
